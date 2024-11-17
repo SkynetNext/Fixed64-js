d82d1f9e8205963359da3f89aaa04c70c0c94143
zhuguangwen <zhuguangwen@zealshow.com>
2024-11-18 00:56:15 +0800

    remove build/

--- src/cpp/build.bat
+++ src/cpp/build.bat
@@ -1,16 +1,16 @@
 @echo off
 setlocal
 
-REM 设置工具的相对路径
-SET NINJA_PATH=..\\..\\tools\\ninja\\
-SET CMAKE_PATH=..\\..\\tools\\cmake\\bin
+REM Set the relative paths for the tools
+SET NINJA_PATH=..\\..\\tools\\ninja
+SET CMAKE_PATH=..\\..\\tools\\cmake\\\\bin
 
-REM 将Ninja和CMake的路径添加到PATH中，以确保cmake能找到它们
-set PATH=%CD%\\%NINJA_PATH%;%CD%\\%CMAKE_PATH%;%PATH%
+REM Add Ninja and CMake paths to PATH to ensure cmake can find them
+set PATH=%CD%\\\\%NINJA_PATH%;%CD%\\\\%CMAKE_PATH%;%PATH%
 
 set WORKSPACE=.
 
-REM 确保EMSDK环境变量已经设置
+REM Ensure EMSDK environment variable is set
 echo EMSDK=%EMSDK%
 if "%EMSDK%"=="" (
     echo Emscripten SDK is not installed or not set up correctly.
@@ -27,11 +27,11 @@ goto :eof
 setlocal
 set ORIGINAL_DIR=%CD%
 
-REM 创建构建目录并进入
+REM Create the build directory and enter it
 set BUILD_DIR=build
 if not exist %BUILD_DIR% mkdir %BUILD_DIR%
 cd %BUILD_DIR%
-REM 使用call执行emcmake和cmake命令
+REM Use call to execute emcmake and cmake commands
 call emcmake cmake -GNinja ..
 if %errorlevel% neq 0 (
     cd %ORIGINAL_DIR%
